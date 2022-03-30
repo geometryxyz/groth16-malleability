e4e8852f4140c69a52ad54ee371827722ec78164
Andrija <akinovak@gmail.com>
2022-03-30 10:52:53 +0200

    lin_dep with private&pub input

--- circom/circuits/linear_dep.circom
+++ circom/circuits/linear_dep.circom
@@ -1,74 +1,29 @@
 pragma circom 2.0.0;
 
-/*
-(a1 + 2*a2) * a3 = a4
-(a1 + 2*a2) * (a3 - 1) = (a4 - 3)
-
-a1 = 1
-a2 = 1
-
-a3 = 6
-a4 = 18
-
-1. (1 + 2) * 6 = 18
-2. (1 + 2) * 5 = 15
-
-A = ( 
-    0 1 2 0 0 
-    0 1 2 0 0
-    )
-B = (
-    0  0 0 1 0
-    -1 0 0 1 0
-    )
-
-C = (
-    0  0 0 0 1
-    -3 0 0 0 1
-    )
-
-=> 
-
-    2 * u1(x) = u2(x)
-
-    nominator: beta * ui(x) + alpha * vi(x) + wi(x)
-
-    1: beta * u1(x) + 0 + 0 = T -> a1*T
-
-    2: beta * 2 * u1(x) + 0 + 0 -> a2*2*T
-
-    change pi: 
-
-    a1` = (a1 + 2 * a2) -> a1*T + 2*a2*T
-    a2` = 0 -> 0 
-*/
-
-include "../node_modules/circomlib/circuits/poseidon.circom";
+// include "../node_modules/circomlib/circuits/poseidon.circom";
 
 template LDependence() {
     signal input a;
     signal input b;
     signal input c;
     signal input d;
+    signal input e;
 
-    signal input private_inputs_hash;
+    // signal input private_inputs_hash;
 
-    signal d_minus_three; 
-    d_minus_three <== d - 3;
+    signal sixteen_e; 
+    sixteen_e <== 16 * e;
 
-    signal c_minus_one;
-    c_minus_one <== c - 1;
+    e === (2*a + c - 3 + b) * (b + 2);
+    sixteen_e === (b + 2*d + 1) * (6*a + 2*b + 3*c + d);
 
-    d === (a + 2*b) * c;
-    d_minus_three === (a + 2*b) * c_minus_one;
+    // component poseidon = Poseidon(2);
+    // poseidon.inputs[0] <== c;
+    // poseidon.inputs[1] <== d;
 
-    component poseidon = Poseidon(2);
-    poseidon.inputs[0] <== c;
-    poseidon.inputs[1] <== d;
-
-    private_inputs_hash === poseidon.out;
+    // private_inputs_hash === poseidon.out;
 
 }
 
-component main {public [a, b, private_inputs_hash]} = LDependence();
+component main {public [a]} = LDependence();
 
--- circom/test/linear_dep.test.ts
+++ circom/test/linear_dep.test.ts
@@ -5,16 +5,48 @@ import { genProof, verifyProof } from "../src"
 const { buildPoseidon } = require("circomlibjs") 
 
 
-const { ZqField, Scalar } = require("ffjavascript")
+const { ZqField, Scalar, buildBn128, utils } = require("ffjavascript")
+const { unstringifyBigInts, stringifyBigInts } = utils;
 
 const circuit = "linear_dep";
 const wasmFilePath = path.join(config.build.snark, circuit, `${circuit}.wasm`)
 const finalZkeyPath = path.join(config.build.snark, circuit, `${circuit}_final.zkey`)
 const vkeyPath = path.join(config.build.snark, circuit, `${circuit}_verification_key.json`)
 const vKey = JSON.parse(fs.readFileSync(vkeyPath, "utf-8"))
+const binFileUtils = require("@iden3/binfileutils");
 
 const F = new ZqField(Scalar.fromString("21888242871839275222246405745257275088548364400416034343698204186575808495617"));
-const two = F.e("2")
+
+
+const buildMalleabeC = async (stringified_c: string, matching_base_index: number, matching_pub_input: BigInt, new_public_input: BigInt, lf: BigInt) => {
+    const c = unstringifyBigInts(stringified_c)
+
+    const {fd: fdZKey, sections: sectionsZKey} = await binFileUtils.readBinFile(finalZkeyPath, "zkey", 2, 1<<25, 1<<23)
+    const buffBasesC = await binFileUtils.readSection(fdZKey, sectionsZKey, 8)
+
+    const curve = await buildBn128();
+    const Fr = curve.Fr;
+    const G1 = curve.G1;
+
+    const new_pi = new Uint8Array(Fr.n8);
+    Scalar.toRprLE(new_pi, 0, new_public_input, Fr.n8);
+
+    const matching_pub = new Uint8Array(Fr.n8);
+    Scalar.toRprLE(matching_pub, 0, matching_pub_input, Fr.n8);
+
+    const sGIn = curve.G1.F.n8*2
+    const matching_base = buffBasesC.slice(matching_base_index*sGIn, matching_base_index*sGIn + sGIn)
+    
+    const linear_factor = Fr.e(lf.toString(10))
+
+    const delta_lf = Fr.mul(linear_factor, Fr.sub(matching_pub, new_pi));
+
+    const p = await curve.G1.timesScalar(matching_base, delta_lf);
+    const affine_c = G1.fromObject(c);
+
+    const malleable_c = G1.toAffine(G1.add(affine_c, p))
+    return stringifyBigInts(G1.toObject(malleable_c))
+}
 
 describe('Proof test', () => {
     let poseidon: any;
@@ -25,31 +57,34 @@ describe('Proof test', () => {
     it("Should create malleable proof", async () => {
         const a = F.e("1")
         const b = F.e("1")
-        const c = F.e("6")
-        const d = F.e("18")
+        const c = F.e("1")
+        const d = F.e("1")
+        const e = F.e("3")
 
-        const private_inputs_hash_buff = poseidon([c.toString(), d.toString()])
-        const private_inputs_hash = poseidon.F.toString(private_inputs_hash_buff)
+        // const private_inputs_hash_buff = poseidon([c.toString(), d.toString()])
+        // const private_inputs_hash = poseidon.F.toString(private_inputs_hash_buff)
 
-        console.log(private_inputs_hash)
+        // console.log(private_inputs_hash)
 
         const witness = {
             a: a.toString(),
             b: b.toString(),
             c: c.toString(),
             d: d.toString(),
-            private_inputs_hash
+            e: e.toString(),
+            // private_inputs_hash
         };
 
         const fullProof = await genProof(witness, wasmFilePath, finalZkeyPath);
         const validProof = await verifyProof(vKey, fullProof);
         expect(validProof).toBe(true)
 
-        const newB = Scalar.fromString(BigInt("0x32Be343B94f860124dC4fEe278FDCBD38C102D88").toString())
-        const newA = F.add(a, F.mul(two, F.sub(b, newB)))
-
-        fullProof.publicSignals[0] = newA.toString()
-        fullProof.publicSignals[1] = newB;
+        const newPi = BigInt("0x32Be343B94f860124dC4fEe278FDCBD38C102D88")
+        const linearDep = BigInt(2)
+        const matchingBase = 1;
+        const malleable_c = await buildMalleabeC(fullProof.proof.pi_c, matchingBase, BigInt(a), newPi, linearDep)
+        fullProof.proof.pi_c = malleable_c;
+        fullProof.publicSignals[0] = newPi
 
         const malleableProof = await verifyProof(vKey, fullProof);
         expect(malleableProof).toBe(true)
