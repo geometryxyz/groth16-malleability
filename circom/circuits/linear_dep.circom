cdd96b9a7c2f49e6f42f49b18d8601417451224c
Koh Wei Jie <contact@kohweijie.com>
2022-07-19 08:18:53 -0700

    re-enabled poseidon in the circuit to try to fix the issue; wip

--- circom/circuits/linear_dep.circom
+++ circom/circuits/linear_dep.circom
@@ -1,5 +1,36 @@
 pragma circom 2.0.0;
 
+include "../node_modules/circomlib/circuits/poseidon.circom";
+
+template LDependence() {
+    signal input a;
+    signal input b;
+    signal input c;
+    signal input d;
+    signal input e;
+
+    signal input private_inputs_hash;
+
+    signal sixteen_e; 
+    sixteen_e <== 16 * e;
+
+    e === (2*a + c - 3 + b) * (b + 2);
+    sixteen_e === (b + 2*d + 1) * (6*a + 2*b + 3*c + d);
+
+    component poseidon = Poseidon(4);
+    poseidon.inputs[0] <== b;
+    poseidon.inputs[1] <== c;
+    poseidon.inputs[2] <== d;
+    poseidon.inputs[3] <== e;
+
+    private_inputs_hash === poseidon.out;
+}
+
+component main {public [a, private_inputs_hash]} = LDependence();
+
+/*
+pragma circom 2.0.0;
+
 // include "../node_modules/circomlib/circuits/poseidon.circom";
 
 template LDependence() {
@@ -26,4 +57,5 @@ template LDependence() {
 }
 
 component main {public [a]} = LDependence();
+*/
 
--- circom/test/linear_dep.test.ts
+++ circom/test/linear_dep.test.ts
@@ -1,3 +1,4 @@
+jest.setTimeout(30000);
 import { config } from "../package.json"
 import path from "path"
 import fs from "fs"
@@ -71,10 +72,10 @@ describe('Proof test', () => {
         const d = F.e("1")
         const e = F.e("3")
 
-        // const private_inputs_hash_buff = poseidon([c.toString(), d.toString()])
-        // const private_inputs_hash = poseidon.F.toString(private_inputs_hash_buff)
+        const private_inputs_hash_buff = poseidon([b, c, d, e].map((x) => x.toString()));
+        const private_inputs_hash = poseidon.F.toString(private_inputs_hash_buff)
 
-        // console.log(private_inputs_hash)
+        //console.log(private_inputs_hash)
 
         const witness = {
             a: a.toString(),
@@ -82,12 +83,12 @@ describe('Proof test', () => {
             c: c.toString(),
             d: d.toString(),
             e: e.toString(),
-            // private_inputs_hash
+            private_inputs_hash
         };
 
         const fullProof = await genProof(witness, wasmFilePath, finalZkeyPath);
-        const validProof = await verifyProof(vKey, fullProof);
-        expect(validProof).toBe(true)
+        const isValidBefore = await verifyProof(vKey, fullProof);
+        expect(isValidBefore).toBe(true)
 
         const newPi = BigInt("0x32Be343B94f860124dC4fEe278FDCBD38C102D88")
         const linearDep = BigInt(2)
@@ -98,44 +99,48 @@ describe('Proof test', () => {
             BigInt(a),
             newPi,
             linearDep,
-            1,
+            2, // num_pub_inputs
             finalZkeyPath,
-        )
-        fullProof.proof.pi_c = malleable_c;
-        fullProof.publicSignals[0] = newPi
-
-        const malleableProof = await verifyProof(vKey, fullProof);
-        expect(malleableProof).toBe(true)
-    }, 30000)
-
-    it("Should create malleable proof from hardcoded proof", async () => {
-        const zkey2Path = path.join(config.build.snark, circuit, `zkey2.zkey`)
-        const vkeyPath = path.join(config.build.snark, circuit, `vkey2.json`)
-        const vKey = JSON.parse(fs.readFileSync(vkeyPath, "utf-8"))
-
-        const proofStr = '{"proof":{"pi_a":["9116137692168313545257963496822789078889884338884935318299769578609942372913","1169049148027571100028615690504001146545764669525597079739831676817269574802","1"],"pi_b":[["18173624827777478757465348127607695706737142597392275714133616708574906221118","6248999832388094105037078891358214271529600414771987765785754489664451781710"],["1742412230559230372842295749896283403724596008502928310361616441132352244059","10079016627411999768171919959229700402776116097832870406016675610441869045550"],["1","0"]],"pi_c":["8442490429681697496656183243861064363210289740000793944498285667066888569225","15670531465640369709154663231318828218560812151060147954011736913669460346783","1"],"protocol":"groth16","curve":"bn128"},"publicSignals":["1","16500345345413527311975906972246791795787167817628578714941278197073435252369"]}'
-
-        const fullProof = unstringifyBigInts(JSON.parse(proofStr));
-        const isValidBefore = await verifyProof(vKey, fullProof);
-        expect(isValidBefore).toBe(true)
-
-        const a = 1;
-        const newPi = BigInt("0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266")
-        const linearDep = BigInt(2)
-        const matchingBaseIndex = 1;
-        const malleable_c = await buildMalleabeC(
-            fullProof.proof.pi_c,
-            matchingBaseIndex,
-            BigInt(a),
-            newPi,
-            linearDep,
-            2,
-            zkey2Path,
         )
         fullProof.proof.pi_c = malleable_c;
         fullProof.publicSignals[0] = newPi
 
         const isValidAfter = await verifyProof(vKey, fullProof);
         expect(isValidAfter).toBe(true)
-    })
+    }, 30000)
+
+    //it("Should create malleable proof from hardcoded proof", async () => {
+        //const zkey2Path = path.join(config.build.snark, circuit, `zkey2.zkey`)
+        //const vkeyPath = path.join(config.build.snark, circuit, `vkey2.json`)
+        //const vKey = JSON.parse(fs.readFileSync(vkeyPath, "utf-8"))
+        
+        //// This proof comes from the groth-puzzle repository. It's here to test
+        //// whether buildMalleabeC works on any proof, not just the one
+        //// generated for linear_dep.circom
+
+        //const proofStr = '{"proof":{"pi_a":["9116137692168313545257963496822789078889884338884935318299769578609942372913","1169049148027571100028615690504001146545764669525597079739831676817269574802","1"],"pi_b":[["18173624827777478757465348127607695706737142597392275714133616708574906221118","6248999832388094105037078891358214271529600414771987765785754489664451781710"],["1742412230559230372842295749896283403724596008502928310361616441132352244059","10079016627411999768171919959229700402776116097832870406016675610441869045550"],["1","0"]],"pi_c":["8442490429681697496656183243861064363210289740000793944498285667066888569225","15670531465640369709154663231318828218560812151060147954011736913669460346783","1"],"protocol":"groth16","curve":"bn128"},"publicSignals":["1","16500345345413527311975906972246791795787167817628578714941278197073435252369"]}'
+
+        //const fullProof = unstringifyBigInts(JSON.parse(proofStr));
+        //const isValidBefore = await verifyProof(vKey, fullProof);
+        //expect(isValidBefore).toBe(true)
+
+        //const a = 1;
+        //const newPi = BigInt("0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266")
+        //const linearDep = BigInt(2)
+        //const matchingBaseIndex = 1;
+        //const malleable_c = await buildMalleabeC(
+            //fullProof.proof.pi_c,
+            //matchingBaseIndex,
+            //BigInt(a),
+            //newPi,
+            //linearDep,
+            //2,
+            //zkey2Path,
+        //)
+        //fullProof.proof.pi_c = malleable_c;
+        //fullProof.publicSignals[0] = newPi
+
+        //const isValidAfter = await verifyProof(vKey, fullProof);
+        //expect(isValidAfter).toBe(true)
+    //})
 })
